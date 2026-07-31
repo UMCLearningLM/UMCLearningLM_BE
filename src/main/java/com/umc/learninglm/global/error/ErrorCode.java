b055df1a92051f79a1372a1d1b7f0126225919e9
YANG JUNHYEOK <wnsgur5152@naver.com>
2026-07-31 14:01:34 +0900

    Fix formatting of TUTORIAL_ALREADY_COMPLETED error

--- src/main/java/com/umc/learninglm/global/error/ErrorCode.java
+++ src/main/java/com/umc/learninglm/global/error/ErrorCode.java
@@ -67,7 +67,7 @@ public enum ErrorCode {
 	TUTORIAL_STEPS_NOT_FOUND(HttpStatus.NOT_FOUND, "TUTORIAL40403", "튜토리얼 단계 정보가 없습니다."),
 	TUTORIAL_ALREADY_SAVED(HttpStatus.CONFLICT, "TUTORIAL40901", "이미 저장된 튜토리얼입니다."),
 	TUTORIAL_ALREADY_STARTED(HttpStatus.CONFLICT, "TUTORIAL40902", "이미 다른 flow로 진행 중인 튜토리얼입니다."),
-	TUTORIAL_ALREADY_COMPLETED(HttpStatus.CONFLICT, "TUTORIAL40903", "이미 완료한 튜토리얼입니다.");
+	TUTORIAL_ALREADY_COMPLETED(HttpStatus.CONFLICT, "TUTORIAL40903", "이미 완료한 튜토리얼입니다."),
 
     // Home
     HOME_CATEGORY_CONFIGURATION_MISSING(HttpStatus.INTERNAL_SERVER_ERROR, "HOME50001","홈 화면 필수 카테고리 데이터가 누락되었습니다."),
